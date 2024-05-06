a4c52e1a9460fed4ab0a379e7b628a3ec7f68c1f
oyoshot <105966658+oyoshot@users.noreply.github.com>
2024-05-06 16:58:13 +0900

    Device/mac (#42)
    * Update .config/nvim/lazy-lock.json
    
    * Update .config/nvim/lazy-lock.json
    
    * Update .config/nvim/lazy-lock.json
    Update .config/nvim/lua/core/lazyvim.lua
    Add .config/nvim/lua/plugins/barbar.lua
    Update .config/nvim/lua/plugins/alpha.lua
    Update .config/nvim/lua/plugins/lualine.lua
    Update .config/nvim/lua/plugins/telescope.lua
    Add .config/nvim/lua/plugins/hlchank.lua
    
    * Update .config/nvim/lua/plugins/lualine.lua

--- dot_config/nvim/lua/plugins/executable_telescope.lua
+++ dot_config/nvim/lua/plugins/executable_telescope.lua
@@ -1,42 +1,62 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  cmd = {
-    "Telescope",
-  },
-  event = { "BufReadPre", "BufNewFile" },
-  keys = {
-    { "<leader>m", "<cmd>Telescope marks<cr>",      desc = "search by [M]arks" },
-    { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "search [F]iles" },
-    { "<leader>g", "<cmd>Telescope live_grep<cr>",  desc = "search by [G]rep" },
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    telescope.setup({
-      defaults = {
-        path_display = { "truncate " },
-        mappings = {
-          i = {
-            ['<C-u>'] = false,
-            ['<C-d>'] = false,
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
+    {
+        "nvim-telescope/telescope.nvim",
+        cmd = {
+            "Telescope",
         },
-      },
-    })
-    telescope.load_extension("fzf")
-    -- See `:help telescope.builtin`
-    -- vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'search [F]iles' })
-    -- vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'search by [G]rep' })
-    -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-    -- vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing [B]uffers' })
-    vim.keymap.set('n', '<leader>H', require('telescope.builtin').help_tags, { desc = 'search [H]elp' })
-    vim.keymap.set('n', '<leader>w', require('telescope.builtin').grep_string, { desc = 'search current [W]ord' })
-    -- vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-  end,
+        event = { "BufReadPre", "BufNewFile" },
+        keys = {
+            { "<leader>m", "<cmd>Telescope marks<cr>",      desc = "search by [M]arks" },
+            { "<leader>f", "<cmd>Telescope find_files<cr>", desc = "search [F]iles" },
+            { "<leader>g", "<cmd>Telescope live_grep<cr>",  desc = "search by [G]rep" },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            "nvim-tree/nvim-web-devicons",
+            "prochri/telescope-all-recent.nvim",
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            telescope.setup({
+                defaults = {
+                    path_display = { "truncate " },
+                    mappings = {
+                        i = {
+                            ["<C-u>"] = false,
+                            ["<C-d>"] = false,
+                            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                        },
+                    },
+                },
+            })
+            telescope.load_extension("fzf")
+            -- See `:help telescope.builtin`
+            -- vim.keymap.set('n', '<leader>f', require('telescope.builtin').find_files, { desc = 'search [F]iles' })
+            -- vim.keymap.set('n', '<leader>g', require('telescope.builtin').live_grep, { desc = 'search by [G]rep' })
+            -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+            -- vim.keymap.set('n', '<leader>b', require('telescope.builtin').buffers, { desc = 'Find existing [B]uffers' })
+            vim.keymap.set("n", "<leader>H", require("telescope.builtin").help_tags, { desc = "search [H]elp" })
+            vim.keymap.set(
+                "n",
+                "<leader>w",
+                require("telescope.builtin").grep_string,
+                { desc = "search current [W]ord" }
+            )
+            -- vim.keymap.set('n', '<leader>d', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+        end,
+    },
+
+    {
+        "prochri/telescope-all-recent.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "nvim-telescope/telescope.nvim",
+            "kkharji/sqlite.lua",
+            -- optional, if using telescope for vim.ui.select
+            -- "stevearc/dressing.nvim",
+        },
+        opts = {},
+    },
 }
