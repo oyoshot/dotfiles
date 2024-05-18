864cf173c1197ec09b32561857c40bbe8bc01505
oyoshot <oyoshot@gmail.com>
2024-05-18 18:31:42 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp-zero.lua Add .config/nvim/lua/plugins/go.lua Add .config/nvim/lua/plugins/nvim-cmp.lua Update .config/nvim/lua/plugins/nvim-dap.lua

--- dot_config/nvim/lua/plugins/nvim-dap.lua
+++ dot_config/nvim/lua/plugins/nvim-dap.lua
@@ -1,9 +1,17 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"theHamsta/nvim-dap-virtual-text",
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			"theHamsta/nvim-dap-virtual-text",
+		},
+		lazy = true,
+		config = function() end,
 	},
-	lazy = true,
-	config = function() end,
+
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		lazy = true,
+	},
 }
