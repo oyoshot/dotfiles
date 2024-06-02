2bd79ff6a87252ef21031b817c93a5a0a444201d
oyoshot <105966658+oyoshot@users.noreply.github.com>
2024-06-02 12:54:32 +0900

    Device/mac (#48)
    * Update .config/nvim/lazy-lock.json
    Update .config/nvim/lua/plugins/diffview.lua
    Update .config/nvim/lua/plugins/mini.lua
    Add .config/nvim/lua/plugins/flash.lua
    Remove .config/nvim/lua/plugins/leap.lua
    Add .config/nvim/lua/plugins/vim-traslator.lua
    
    * Add .config/memo/config.toml
    
    * Update .config/zsh/defer.zsh
    
    * To zsh
    
    * Update .config/homebrew/Brewfile
    
    * Update .config/memo/config.toml
    Update 2_install-go.zsh
    
    * Update .config/memo/config.toml
    Update 2_install-go.zsh
    
    * Update .config/memo/config.toml
    
    * Update .config/memo/config.toml
    Add .config/memo/plugins/commit
    Update 2_install-go.zsh
    
    * Update .config/memo/plugins/commit
    Update .config/yay/extra-packeges
    Update 2_install-go.zsh
    
    * Update .config/homebrew/Brewfile
    Update .config/mise/config.toml
    
    * Update .config/homebrew/Brewfile

--- dot_config/nvim/lua/plugins/executable_mini.lua
+++ dot_config/nvim/lua/plugins/executable_mini.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    'echasnovski/mini.indentscope',
-    version = '*',
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require('mini.indentscope').setup(
-        {
-          -- Which character to use for drawing scope indicator
-          -- '│', '|', '¦', '┆', '┊', ''
-          symbol = '│',
-        }
-      )
-    end
-  },
+	{
+		--'echasnovski/mini.indentscope',
+		--version = '*',
+		--event = { "BufReadPre", "BufNewFile" },
+		--config = function()
+		--  require('mini.indentscope').setup(
+		--    {
+		--      -- Which character to use for drawing scope indicator
+		--      -- '│', '|', '¦', '┆', '┊', ''
+		--      symbol = '│',
+		--    }
+		--  )
+		--end
+	},
 }
