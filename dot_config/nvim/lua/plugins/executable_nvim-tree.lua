5bdbf70bb6475b08d6e0b88940810bca675e961d
oyoshot <oyoshot@gmail.com>
2024-08-20 20:53:54 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/core/keymaps.lua Update .config/nvim/lua/plugins/lsp-zero.lua Update .config/nvim/lua/plugins/nvim-tree.lua
    Update jj

--- dot_config/nvim/lua/plugins/executable_nvim-tree.lua
+++ dot_config/nvim/lua/plugins/executable_nvim-tree.lua
@@ -1,50 +1,13 @@
 return {
-	keys = {
-		{ "<C-n>", "<cmd>NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer" },
-		{
-			"n",
-			"<leader>ef",
-			"<cmd>NvimTreeFindFileToggle<CR>",
-			desc = "Toggle file explorer on current file",
-		},
-		{ "n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", desc = "Collapse file explorer" },
-		{ "n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", desc = "Refresh file explorer" },
-	},
 	"nvim-tree/nvim-tree.lua",
+	version = "*",
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
+	keys = {
+		{ "<C-n>", "<cmd>NvimTreeFindFileToggle<CR>", desc = "Toggle file explorer" },
+	},
 	config = function()
-		local nvimtree = require("nvim-tree")
-
-		-- configure nvim-tree
-		nvimtree.setup({
-			view = {
-				width = 35,
-				relativenumber = false,
-			},
-			-- change folder arrow icons
-			renderer = {
-				indent_markers = {
-					enable = false,
-				},
-			},
-			-- disable window_picker for
-			-- explorer to work well with
-			-- window splits
-			actions = {
-				open_file = {
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-			filters = {
-				custom = { ".DS_Store" },
-			},
-			git = {
-				ignore = false,
-			},
-		})
+		require("nvim-tree").setup({})
 	end,
 }
