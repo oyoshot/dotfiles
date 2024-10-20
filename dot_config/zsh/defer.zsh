ee6b1bb86c4f621aa93d0b6a3083605134f1fc40
oyoshot <105966658+oyoshot@users.noreply.github.com>
2024-10-21 01:49:00 +0900

    Device/mac (#57)
    * Update .config/alacritty/alacritty.toml
    Update .config/git/conf.d/core.conf
    
    * Update .config/alacritty/alacritty.toml
    
    * Update .config/nvim/lazy-lock.json
    
    * Update .config/mise/config.toml
    Update .config/zsh/defer.zsh
    Update .config/zsh/.zshenv
    
    * Update .config/zsh/.zshenv
    Update .config/zsh/.zshrc
    Add Library/Preferences/atcoder-cli-nodejs/config.json
    Add Library/Preferences/atcoder-cli-nodejs/cpp/main.cpp
    Add Library/Preferences/atcoder-cli-nodejs/cpp/template.json
    Add Library/Preferences/atcoder-cli-nodejs/python/main.py
    Add Library/Preferences/atcoder-cli-nodejs/python/template.json

--- dot_config/zsh/defer.zsh
+++ dot_config/zsh/defer.zsh
@@ -11,7 +11,7 @@
 (( ${+commands[mise]} )) && eval "$(mise activate zsh)"
 #export MISE_GO_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/mise/default-go-packages"
 #export MISE_NODE_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/mise/default-npm-packages"
-export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/mise/default-python-packages"
+#export MISE_PYTHON_DEFAULT_PACKAGES_FILE="$XDG_CONFIG_HOME/mise/default-python-packages"
 
 # Go
 export GO111MODULE="on"
