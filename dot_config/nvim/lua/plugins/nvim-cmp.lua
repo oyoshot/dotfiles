428452e720dcf391dd78c7d00b75884f7bd302a6
oyoshot <105966658+oyoshot@users.noreply.github.com>
2024-10-27 16:50:16 +0900

    `alacritty migrate` / nvim-cmp / install script (#59)
    * Change attributes of .config/alacritty/alacritty.toml
    
    * Update .config/mise/config.toml
    Update .config/nvim/lazy-lock.json
    Update .config/nvim/lua/plugins/nvim-cmp.lua
    Add .config/nvim/startup-mac.log
    
    * Update .config/nvim/lua/plugins/nvim-cmp.lua
    
    Update .config/nvim/lua/plugins/nvim-cmp.lua
    
    * refact: Setup and Initial installation.
    
    refact: Setup and Initial installation.
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation
    
    refact: Setup and Initial installation

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,15 +4,29 @@ return {
 	{ "hrsh7th/cmp-buffer", lazy = true },
 	{ "hrsh7th/cmp-path", lazy = true },
 	{ "hrsh7th/cmp-cmdline", lazy = true },
+	{ "f3fora/cmp-spell", lazy = true },
+	{ "lukas-reineke/cmp-under-comparator", lazy = true },
+	{ "andersevenrud/cmp-tmux", lazy = true },
 	{ "saadparwaiz1/cmp_luasnip", lazy = true },
+	{ "kdheepak/cmp-latex-symbols", lazy = true },
+	{ "ray-x/cmp-treesitter", lazy = true },
 	{ "onsails/lspkind.nvim", lazy = true },
 
 	{
 		"L3MON4D3/LuaSnip",
 		build = "make install_jsregexp",
 		lazy = true,
+		dependencies = { "rafamadriz/friendly-snippets" },
 		config = function()
+			local snippet_path = vim.fn.stdpath("config") .. "/snips/"
+			if not vim.tbl_contains(vim.opt.rtp:get(), snippet_path) then
+				vim.opt.rtp:append(snippet_path)
+			end
+
+			require("luasnip").config.set_config()
+			require("luasnip.loaders.from_lua").lazy_load()
 			require("luasnip.loaders.from_vscode").lazy_load()
+			require("luasnip.loaders.from_snipmate").lazy_load()
 		end,
 	},
 
@@ -34,17 +48,16 @@ return {
 					{ "│", hl },
 				}
 			end
+
 			local lspkind = require("lspkind")
 			local cmp = require("cmp")
 			cmp.setup({
 				preselect = cmp.PreselectMode.None,
 
 				window = {
-					completion = {
-						border = border("PmenuBorder"),
-						winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:PmenuSel",
+					completion = cmp.config.window.bordered({
 						scrollbar = false,
-					},
+					}),
 					documentation = {
 						border = border("CmpDocBorder"),
 						winhighlight = "Normal:CmpDoc",
@@ -63,6 +76,7 @@ return {
 				matching = {
 					disallow_partial_fuzzy_matching = false,
 				},
+
 				performance = {
 					async_budget = 1,
 					max_view_entries = 120,
@@ -94,6 +108,7 @@ return {
 							fallback()
 						end
 					end, { "i", "s" }),
+
 					["<CR>"] = cmp.mapping({
 						i = function(fallback)
 							if cmp.visible() and cmp.get_active_entry() then
@@ -106,11 +121,13 @@ return {
 						c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
 					}),
 				}),
+
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+
 				-- You should specify your *installed* sources.
 				sources = {
 					{ name = "nvim_lsp", max_item_count = 350 },
