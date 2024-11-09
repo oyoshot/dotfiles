f0ec6576ed6062700e1088fb60403baef2d1cbea
oyoshot <oyoshot@gmail.com>
2024-11-09 17:27:17 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/lsp-signature.lua

--- dot_config/nvim/lua/plugins/executable_lsp-signature.lua
+++ dot_config/nvim/lua/plugins/executable_lsp-signature.lua
@@ -2,12 +2,8 @@ return {
 	"ray-x/lsp_signature.nvim",
 	lazy = true,
 	event = "LspAttach",
-	config = function(_, _)
-		require("lsp_signature").setup({
-			bind = true,
-			handler_opts = {
-				border = "rounded",
-			},
-		})
+	opts = {},
+	config = function(_, opts)
+		require("lsp_signature").setup(opts)
 	end,
 }
