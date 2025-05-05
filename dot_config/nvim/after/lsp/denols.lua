4ad885e5b81992ff1ba46c49b87872d66f1c9318
oyoshot <oyoshot@gmail.com>
2025-05-05 15:10:30 +0900

    Add .config/nvim/after/lsp/denols.lua Add .config/nvim/after/lsp/vtsls.lua

---
+++ dot_config/nvim/after/lsp/denols.lua
@@ -0,0 +1,17 @@
+---@type vim.lsp.Config
+return {
+	root_makers = { "deno.json", "deno.jsonc", "deps.ts" },
+	workspace_required = true,
+	settings = {
+		deno = {
+			inlayHints = {
+				parameterNames = { enabled = "all", suppressWhenArgumentMatchesName = true },
+				parameterTypes = { enabled = true },
+				variableTypes = { enabled = true, suppressWhenTypeMatchesName = true },
+				propertyDeclarationTypes = { enabled = true },
+				functionLikeReturnTypes = { enable = true },
+				enumMemberValues = { enabled = true },
+			},
+		},
+	},
+}
