6c1157b71ba2639ebf26aae54fc9230c415b6183
oyoshot <oyoshot@gmail.com>
2025-06-29 15:08:37 +0900

    Update .config/nvim/lazy-lock.json Update .config/nvim/lua/plugins/nvim-cmp.lua Add .config/nvim/lua/plugins/skkeleton.lua

---
+++ dot_config/nvim/lua/plugins/skkeleton.lua
@@ -0,0 +1,50 @@
+local function dict(name, file, enc)
+	local base = vim.fn.stdpath("data") .. "/lazy/"
+	return { base .. name .. "/" .. file, enc }
+end
+
+---@type LazyPluginSpec[]
+return {
+	{ "uasi/skk-emoji-jisyo", name = "skk-emoji-en", lazy = true },
+	{ "ymrl/SKK-JISYO.emoji-ja", name = "skk-emoji-ja", lazy = true },
+
+	{
+		"vim-skk/skkeleton",
+
+		dependencies = {
+			"vim-denops/denops.vim",
+			"aosc-dev/skk-jisyo",
+			"skk-emoji-en",
+			"skk-emoji-ja",
+		},
+
+		event = { "InsertEnter" },
+
+		build = function()
+			local dir = vim.fn.stdpath("data") .. "/skk"
+			vim.fn.mkdir(dir, "p")
+		end,
+
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "skkeleton-initialize-pre",
+
+				callback = function()
+					vim.fn["skkeleton#config"]({
+						eggLikeNewline = true,
+						globalDictionaries = {
+							dict("skk-jisyo", "SKK-JISYO.L", "euc-jp"),
+							dict("skk-emoji-en", "SKK-JISYO.emoji.utf8", "utf-8"),
+							dict("skk-emoji-ja", "SKK-JISYO.emoji-ja.utf8", "utf-8"),
+						},
+						userDictionary = vim.fn.stdpath("data") .. "/skk/user_jisyo",
+					})
+				end,
+			})
+		end,
+
+		config = function()
+			vim.keymap.set({ "i", "c" }, "<C-j>", "<Plug>(skkeleton-toggle)")
+		end,
+	},
+}
