72f49a9da98b6a765e2969eb820d444ca67ea3dc
oyoshot <105966658+oyoshot@users.noreply.github.com>
2025-10-12 15:18:01 +0900

    Lsp/mason (#111)
    * chore: rm mason
    
    * Update .config/mise/config.toml
    
    * Update .config/mise/config.toml
    
    * Update .config/mise/config.toml
    
    * Update .config/nvim/lua/plugins/conform.lua
    
    * Update .config/nvim/lazy-lock.json

--- dot_config/nvim/lua/plugins/navigator.lua
+++ dot_config/nvim/lua/plugins/navigator.lua
@@ -1,15 +1,10 @@
 return {
-    { "ray-x/guihua.lua", run = "cd lua/fzy && make", lazy = true },
+	{ "ray-x/guihua.lua", run = "cd lua/fzy && make", lazy = true },
 
-    {
-        "ray-x/navigator.lua",
-        cond = false,
-        lazy = true,
-        event = "LspAttach",
-        config = function()
-            require("navigator").setup({
-                mason = true,
-            })
-        end,
-    },
+	{
+		"ray-x/navigator.lua",
+		cond = false,
+		lazy = true,
+		event = "LspAttach",
+	},
 }
