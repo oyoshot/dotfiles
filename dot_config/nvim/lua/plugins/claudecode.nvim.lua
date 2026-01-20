2fa992e572fea418a57851ef491f5bcb7c80f1ec
oyoshot <oyoshot@gmail.com>
2026-01-20 14:13:28 +0900

    feat(nvim): add send-and-focus command for claudecode.nvim

--- dot_config/nvim/lua/plugins/claudecode.nvim.lua
+++ dot_config/nvim/lua/plugins/claudecode.nvim.lua
@@ -2,7 +2,21 @@ return {
 	"coder/claudecode.nvim",
 	dependencies = { "folke/snacks.nvim" },
 	lazy = true,
-	config = true,
+	config = function()
+		require("claudecode").setup()
+
+		-- Send and focus command
+		vim.api.nvim_create_user_command("ClaudeCodeSendAndFocus", function(opts)
+			if opts.range > 0 then
+				vim.cmd(string.format("%d,%dClaudeCodeSend", opts.line1, opts.line2))
+			else
+				vim.cmd("ClaudeCodeSend")
+			end
+			vim.defer_fn(function()
+				vim.cmd("ClaudeCodeFocus")
+			end, 100)
+		end, { range = true })
+	end,
 
 	cmd = {
 		"ClaudeCode",
