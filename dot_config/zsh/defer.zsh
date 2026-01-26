9688ca4713a7653f27d1cc2a7b723969439b5982
oyoshot <105966658+oyoshot@users.noreply.github.com>
2026-01-26 19:13:53 +0900

    feat(zsh): add tmux session/window mode to gg (#146)
    * refactor(zsh): extract ghq+fzf chooser for gg
    
    * feat(zsh): add tmux session/window mode to gg
    
    * fix(zsh): make gg tmux sessions unique

--- dot_config/zsh/defer.zsh
+++ dot_config/zsh/defer.zsh
@@ -68,20 +68,106 @@ function g() {
     local root="$(git rev-parse --show-toplevel)" && builtin cd "$root"
 }
 
+function git-by-tmux-fzf() {
+  local root r
+  root=$(ghq root) || return $?
+
+  r=$(
+    ghq list | fzf --tmux 90% \
+      --preview-window="right,40%" \
+      --layout=reverse \
+      --bind='tab:down,shift-tab:up,btab:up' \
+      --preview="git -C '$root/{}' log --color=always"
+  )
+  local fzf_status=$?
+  (( fzf_status == 0 )) || return $fzf_status
+
+  [[ -n "$r" ]] || return 1
+  print -r -- "$root/$r"
+}
+
 function gg() {
-    local root
-    root=$(ghq root) || return $?
-    local r
-    r=$(ghq list | fzf --tmux 90% --preview-window="right,40%" --layout=reverse --bind='tab:down,shift-tab:up,btab:up' --preview="git --git-dir '$root/{}/.git' log --color=always")
-    local fzf_status=$?
-    if [[ $fzf_status -ne 0 ]]; then
-     return $fzf_status
+  local mode="$1" dir
+  dir="$(git-by-tmux-fzf)" || return $?
+
+  if [[ -z "$mode" ]]; then
+    builtin cd -- "$dir"
+    return 0
+  fi
+
+  local url repo
+  url="$(git -C "$dir" remote get-url origin 2>/dev/null)" || url=""
+  if [[ -n "$url" ]]; then
+    repo="${url:t}"
+    repo="${repo%.git}"
+  else
+    repo="${dir:t}"
+  fi
+
+  local branch
+  branch="$(
+    git -C "$dir" symbolic-ref --quiet --short HEAD 2>/dev/null \
+    || git -C "$dir" rev-parse --short HEAD 2>/dev/null
+  )"
+  [[ -n "$branch" ]] || branch="unknown"
+
+  local win
+  win="${repo}:${branch}"
+
+  local owner=""
+  if [[ "$url" == (#b)(*)[:/]([^/:]##)/([^/ ]##)(.git|) ]]; then
+    owner="$match[2]"
+  fi
+
+  local suffix
+  if command -v shasum >/dev/null 2>&1; then
+    suffix="$(print -rn -- "$dir" | shasum | cut -c1-6)"
+  elif command -v sha1sum >/dev/null 2>&1; then
+    suffix="$(print -rn -- "$dir" | sha1sum | cut -c1-6)"
+  else
+    suffix="$(print -rn -- "$dir" | openssl dgst -sha1 | awk '{print $2}' | cut -c1-6)"
+  fi
+
+  local sess_key sess
+  if [[ -n "$owner" ]]; then
+    sess_key="${owner}_${repo}"
+  else
+    sess_key="${dir:h:t}_${repo}"
+  fi
+  sess="${sess_key//[^A-Za-z0-9_-]/_}_${suffix}"
+
+  local in_tmux=0
+  [[ -n "$TMUX" ]] && in_tmux=1
+
+  ensure_session() {
+    tmux has-session -t "$sess" 2>/dev/null && return 0
+    tmux new-session -d -c "$dir" -s "$sess" -n "$win"
+  }
+
+  goto_session() {
+    if (( in_tmux )); then
+      tmux switch-client -t "$sess"
+    else
+      tmux attach -t "$sess"
     fi
-    if [[ -z "$r" ]]; then
-     return 1
-    fi
-    local repository="$root/$r"
-    builtin cd "$repository"
+  }
+
+  case "$mode" in
+    s|session)
+      ensure_session && goto_session
+      ;;
+    w|window)
+      if (( in_tmux )); then
+        tmux new-window -c "$dir" -n "$win"
+      else
+        ensure_session && goto_session
+      fi
+      ;;
+    *)
+      print -u2 -- "usage: gg [s|w]   (no args: cd)"
+      return 2
+      ;;
+  esac
 }
 
 if (( ${+commands[git]} )); then
