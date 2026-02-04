f0d2b6e04adc2637c267a1b01af223ac73011495
oyoshot <oyoshot@gmail.com>
2026-02-04 23:50:37 +0900

    chore(nvim): drop gopls enable from go.nvim config

--- dot_config/nvim/lua/plugins/go.nvim.lua
+++ dot_config/nvim/lua/plugins/go.nvim.lua
@@ -9,7 +9,6 @@ return {
 	ft = { "go", "gomod" },
 	config = function()
 		require("go").setup()
-		vim.lsp.enable("gopls")
 	end,
 	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
