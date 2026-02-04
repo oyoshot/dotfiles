a180ce0f69007434af549caf94372094966e80d9
oyoshot <oyoshot@gmail.com>
2026-02-04 23:47:38 +0900

    chore(nvim): remove CmdlineEnter event for go.nvim

--- dot_config/nvim/lua/plugins/go.nvim.lua
+++ dot_config/nvim/lua/plugins/go.nvim.lua
@@ -11,6 +11,5 @@ return {
 		require("go").setup()
 		vim.lsp.enable("gopls")
 	end,
-	event = { "CmdlineEnter" },
 	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 }
