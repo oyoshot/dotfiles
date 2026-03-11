98b87daec9f9db7f7b5ca059081f2caa99696a46
oyoshot <oyoshot@gmail.com>
2026-03-11 14:57:46 +0900

    style(zsh): reformat defer script for consistent indentation

--- dot_config/zsh/defer.zsh
+++ dot_config/zsh/defer.zsh
@@ -2,13 +2,16 @@
 
 # Deno
 _cpu_count() {
-  if command -v getconf >/dev/null; then getconf _NPROCESSORS_ONLN
-  elif command -v nproc   >/dev/null; then nproc
-  elif [[ "$OSTYPE" == darwin* ]];  then sysctl -n hw.ncpu
-  else echo 1; fi
+    if command -v getconf >/dev/null; then
+        getconf _NPROCESSORS_ONLN
+    elif command -v nproc >/dev/null; then
+        nproc
+    elif [[ "$OSTYPE" == darwin* ]]; then
+        sysctl -n hw.ncpu
+    else echo 1; fi
 }
 
-export DENO_JOBS=$(( $(_cpu_count) + 1 ))
+export DENO_JOBS=$(($(_cpu_count) + 1))
 
 # Go
 export GO111MODULE="on"
@@ -49,146 +52,146 @@ export GNUPGHOME="$XDG_CONFIG_HOME/gnupg"
 #export TF_LOG_PATH="$XDG_DATA_HOME/terraform/terraform.log"
 
 __load_github_token_once() {
-  if [[ -n "${MISE_GITHUB_TOKEN:-}" ]]; then
-    return 0
-  fi
-  if [[ -n "${GITHUB_TOKEN:-}" ]]; then
-    export MISE_GITHUB_TOKEN="$GITHUB_TOKEN"
-    return 0
-  fi
-  (( $+commands[gh] )) || return 0
+    if [[ -n "${MISE_GITHUB_TOKEN:-}" ]]; then
+        return 0
+    fi
+    if [[ -n "${GITHUB_TOKEN:-}" ]]; then
+        export MISE_GITHUB_TOKEN="$GITHUB_TOKEN"
+        return 0
+    fi
+    (($+commands[gh])) || return 0
 
-  local token
-  token="$(command gh auth token 2>/dev/null)" || return 0
-  [[ -n "$token" ]] || return 0
+    local token
+    token="$(command gh auth token 2>/dev/null)" || return 0
+    [[ -n "$token" ]] || return 0
 
-  export MISE_GITHUB_TOKEN="$token"
+    export MISE_GITHUB_TOKEN="$token"
 }
 
 function git-by-tmux-fzf() {
-  local root r
-  root=$(ghq root) || return $?
+    local root r
+    root=$(ghq root) || return $?
 
-  r=$(
-    ghq list | fzf --tmux 90% \
-      --preview-window="right,40%" \
-      --layout=reverse \
-      --bind='tab:down,shift-tab:up,btab:up' \
-      --preview="git -C '$root/{}' log --color=always"
-  )
-  local fzf_status=$?
-  (( fzf_status == 0 )) || return $fzf_status
+    r=$(
+        ghq list | fzf --tmux 90% \
+            --preview-window="right,40%" \
+            --layout=reverse \
+            --bind='tab:down,shift-tab:up,btab:up' \
+            --preview="git -C '$root/{}' log --color=always"
+    )
+    local fzf_status=$?
+    ((fzf_status == 0)) || return $fzf_status
 
-  [[ -n "$r" ]] || return 1
-  print -r -- "$root/$r"
+    [[ -n "$r" ]] || return 1
+    print -r -- "$root/$r"
 }
 
 function gg() {
-  local mode="$1" dir
-  dir="$(git-by-tmux-fzf)" || return $?
+    local mode="$1" dir
+    dir="$(git-by-tmux-fzf)" || return $?
 
-  if [[ -z "$mode" ]]; then
-    builtin cd -- "$dir"
-    return 0
-  fi
-
-  local url repo
-  url="$(git -C "$dir" remote get-url origin 2>/dev/null)" || url=""
-  if [[ -n "$url" ]]; then
-    repo="${url:t}"
-    repo="${repo%.git}"
-  else
-    repo="${dir:t}"
-  fi
-
-  local branch
-  branch="$(
-    git -C "$dir" symbolic-ref --quiet --short HEAD 2>/dev/null \
-    || git -C "$dir" rev-parse --short HEAD 2>/dev/null
-  )"
-  [[ -n "$branch" ]] || branch="unknown"
-
-  local win
-  win="${repo}:${branch}"
-
-  local owner=""
-  if [[ "$url" == (#b)(*)[:/]([^/:]##)/([^/ ]##)(.git|) ]]; then
-    owner="$match[2]"
-  fi
-
-  local suffix
-  if command -v shasum >/dev/null 2>&1; then
-    suffix="$(print -rn -- "$dir" | shasum | cut -c1-6)"
-  elif command -v sha1sum >/dev/null 2>&1; then
-    suffix="$(print -rn -- "$dir" | sha1sum | cut -c1-6)"
-  else
-    suffix="$(print -rn -- "$dir" | openssl dgst -sha1 | awk '{print $2}' | cut -c1-6)"
-  fi
-
-  local sess_key sess
-  if [[ -n "$owner" ]]; then
-    sess_key="${owner}_${repo}"
-  else
-    sess_key="${dir:h:t}_${repo}"
-  fi
-  sess="${sess_key//[^A-Za-z0-9_-]/_}_${suffix}"
-
-  local in_tmux=0
-  [[ -n "$TMUX" ]] && in_tmux=1
-
-  ensure_session() {
-    tmux has-session -t "$sess" 2>/dev/null && return 0
-    tmux new-session -d -c "$dir" -s "$sess" -n "$win"
-  }
-
-  goto_session() {
-    if (( in_tmux )); then
-      tmux switch-client -t "$sess"
-    else
-      tmux attach -t "$sess"
+    if [[ -z "$mode" ]]; then
+        builtin cd -- "$dir"
+        return 0
     fi
-  }
 
-  case "$mode" in
-    s|session)
-      ensure_session && goto_session
-      ;;
-    w|window)
-      if (( in_tmux )); then
-        tmux new-window -c "$dir" -n "$win"
-      else
+    local url repo
+    url="$(git -C "$dir" remote get-url origin 2>/dev/null)" || url=""
+    if [[ -n "$url" ]]; then
+        repo="${url:t}"
+        repo="${repo%.git}"
+    else
+        repo="${dir:t}"
+    fi
+
+    local branch
+    branch="$(
+        git -C "$dir" symbolic-ref --quiet --short HEAD 2>/dev/null ||
+            git -C "$dir" rev-parse --short HEAD 2>/dev/null
+    )"
+    [[ -n "$branch" ]] || branch="unknown"
+
+    local win
+    win="${repo}:${branch}"
+
+    local owner=""
+    if [[ "$url" == (#b)(*)[:/]([^/:]##)/([^/ ]##)(.git|) ]]; then
+        owner="$match[2]"
+    fi
+
+    local suffix
+    if command -v shasum >/dev/null 2>&1; then
+        suffix="$(print -rn -- "$dir" | shasum | cut -c1-6)"
+    elif command -v sha1sum >/dev/null 2>&1; then
+        suffix="$(print -rn -- "$dir" | sha1sum | cut -c1-6)"
+    else
+        suffix="$(print -rn -- "$dir" | openssl dgst -sha1 | awk '{print $2}' | cut -c1-6)"
+    fi
+
+    local sess_key sess
+    if [[ -n "$owner" ]]; then
+        sess_key="${owner}_${repo}"
+    else
+        sess_key="${dir:h:t}_${repo}"
+    fi
+    sess="${sess_key//[^A-Za-z0-9_-]/_}_${suffix}"
+
+    local in_tmux=0
+    [[ -n "$TMUX" ]] && in_tmux=1
+
+    ensure_session() {
+        tmux has-session -t "$sess" 2>/dev/null && return 0
+        tmux new-session -d -c "$dir" -s "$sess" -n "$win"
+    }
+
+    goto_session() {
+        if ((in_tmux)); then
+            tmux switch-client -t "$sess"
+        else
+            tmux attach -t "$sess"
+        fi
+    }
+
+    case "$mode" in
+    s | session)
         ensure_session && goto_session
-      fi
-      ;;
+        ;;
+    w | window)
+        if ((in_tmux)); then
+            tmux new-window -c "$dir" -n "$win"
+        else
+            ensure_session && goto_session
+        fi
+        ;;
     *)
-      print -u2 -- "usage: gg [s|w]   (no args: cd)"
-      return 2
-      ;;
-  esac
+        print -u2 -- "usage: gg [s|w]   (no args: cd)"
+        return 2
+        ;;
+    esac
 }
 
-if (( ${+commands[git]} )); then
-  eval "$(git wt --init zsh)"
+if ((${+commands[git]})); then
+    eval "$(git wt --init zsh)"
 fi
 
 function git-wtx() {
-  local target
-  target="$(
-    git wt |
-      tail -n +2 |
-      fzf --tmux 90% --ansi --exit-0 --layout=reverse --info=hidden --no-multi \
-        --preview-window='right,40%' \
-        --bind='tab:down,shift-tab:up,btab:up' \
-        --preview='echo {} | awk '\''{print $NF}'\'' | xargs git show --color=always' |
-      awk '{print $(NF-1)}'
-  )" || return $?
-  [[ -n "$target" ]] || return 1
-  git wt "$target"
+    local target
+    target="$(
+        git wt |
+            tail -n +2 |
+            fzf --tmux 90% --ansi --exit-0 --layout=reverse --info=hidden --no-multi \
+                --preview-window='right,40%' \
+                --bind='tab:down,shift-tab:up,btab:up' \
+                --preview='echo {} | awk '\''{print $NF}'\'' | xargs git show --color=always' |
+            awk '{print $(NF-1)}'
+    )" || return $?
+    [[ -n "$target" ]] || return 1
+    git wt "$target"
 }
 
 function awsc() {
-  local src=$(aws-vault list --profiles | fzf --tmux 90%)
-  echo "
+    local src=$(aws-vault list --profiles | fzf --tmux 90%)
+    echo "
 Vim にすれば...
 生産性向上！
 健康！
@@ -216,33 +219,32 @@ Vim にすれば...
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⢿⠂⠀⠀⠀⠈⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⢹⢶⡙⣮⠟⠁⠀
 ⠀⠀    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⠿⢛⡿⠿⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⢸⡧⠟⠁⠀⠀⠀
 "
-  [ -n "$src" ] && aws-vault exec "$src" "$@"
+    [ -n "$src" ] && aws-vault exec "$src" "$@"
 }
 
 function cdd() {
-  local directory=$(
-    fd -t d \
-    | fzf --tmux 90%
-  )
-  [ -n "$directory" ] && cd $directory
+    local directory=$(
+        fd -t d |
+            fzf --tmux 90%
+    )
+    [ -n "$directory" ] && cd $directory
 }
 
 ## キーバインド
 
 bindkey '^[' _ghq-fzf
 function _ghq-fzf() {
-  local repository=$(ghq list --full-path --vcs git | fzf --tmux 90%)
-  if [ -n "$repository" ]; then
-    BUFFER="cd \"$repository\""
-    zle accept-line
-  fi
+    local repository=$(ghq list --full-path --vcs git | fzf --tmux 90%)
+    if [ -n "$repository" ]; then
+        BUFFER="cd \"$repository\""
+        zle accept-line
+    fi
 
-  zle -R -c
+    zle -R -c
 }
 zle -N _ghq-fzf
 
 ## エイリアス
-
 
 # sudo の後のコマンドでエイリアスを有効にする
 alias sudo='sudo '
@@ -253,79 +255,80 @@ alias -g G='| grep'
 
 # C で標準出力をクリップボードにコピーする
 # mollifier delta blog : http://mollifier.hatenablog.com/entry/20100317/p1
-if which pbcopy >/dev/null 2>&1 ; then
+if which pbcopy >/dev/null 2>&1; then
     # Mac
     alias -g C='| pbcopy'
-elif which xsel >/dev/null 2>&1 ; then
+elif which xsel >/dev/null 2>&1; then
     # Linux
     alias -g C='| xsel --input --clipboard'
-elif which putclip >/dev/null 2>&1 ; then
+elif which putclip >/dev/null 2>&1; then
     # Cygwin
     alias -g C='| putclip'
 fi
 
 # tar
 tgz() {
-  env COPYFILE_DISABLE=1 tar zcvf "$1" --exclude=".DS_Store" "${@:2}"
+    env COPYFILE_DISABLE=1 tar zcvf "$1" --exclude=".DS_Store" "${@:2}"
 }
 
 # ls color
-if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
-  LS_COLORS="${LS_COLORS}:ow=01;34"; export LS_COLORS
-  export PATH=$PATH:/mnt/c/windows
-  alias II='explorer.exe'
+if grep -qE "(Microsoft|WSL)" /proc/version &>/dev/null; then
+    LS_COLORS="${LS_COLORS}:ow=01;34"
+    export LS_COLORS
+    export PATH=$PATH:/mnt/c/windows
+    alias II='explorer.exe'
 fi
 
-if (( ${+commands[brew]} )); then
-  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-  # rip
-  export GRAVEYARD="~/.Trash"
+if ((${+commands[brew]})); then
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    # rip
+    export GRAVEYARD="~/.Trash"
 fi
 
 # Zoxide
-(( ${+commands[zoxide]} )) && eval "$(zoxide init zsh)"
+((${+commands[zoxide]})) && eval "$(zoxide init zsh)"
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-(( ${+commands[fzf]} )) && source <(fzf --zsh)
+((${+commands[fzf]})) && source <(fzf --zsh)
 
 # anyframe
 autoload -Uz anyframe-init && anyframe-init
 
-if (( ${+TMUX} && $+commands[tmux] )); then
-  VISUAL='tmux display-popup -E -h 70% -w 70% -d '#{pane_current_path}' nvim'
+if ((${+TMUX} && $+commands[tmux])); then
+    VISUAL='tmux display-popup -E -h 70% -w 70% -d '#{pane_current_path}' nvim'
 else
-  VISUAL='nvim'
+    VISUAL='nvim'
 fi
 export VISUAL FCEDIT=$VISUAL
 
 autoload -Uz edit-command-line
-zle      -N  edit-command-line
+zle -N edit-command-line
 
 bindkey -M vicmd 'jj' edit-command-line
 bindkey -M viins 'jj' edit-command-line
 
 # zeno.zsh
 export ZENO_DISABLE_EXECUTE_CACHE_COMMAND=1
-(( ${+commands[bat]} )) && export ZENO_GIT_CAT="bat --color=always"
-(( ${+commands[eza]} )) && export ZENO_GIT_TREE="eza --tree"
+((${+commands[bat]})) && export ZENO_GIT_CAT="bat --color=always"
+((${+commands[eza]})) && export ZENO_GIT_TREE="eza --tree"
 source "$ZDOTDIR/plugins/zeno/zeno.zsh"
 
 if [[ -n $ZENO_LOADED ]]; then
-  bindkey ' '  zeno-auto-snippet
+    bindkey ' ' zeno-auto-snippet
 
-  # autosuggestions をクリアしてから zeno を実行
-  function _zeno-accept-line-clear-suggest() {
-    zle autosuggest-clear 2>/dev/null || true
-    zle zeno-auto-snippet-and-accept-line
-  }
-  zle -N _zeno-accept-line-clear-suggest
-  bindkey '^m' _zeno-accept-line-clear-suggest
+    # autosuggestions をクリアしてから zeno を実行
+    function _zeno-accept-line-clear-suggest() {
+        zle autosuggest-clear 2>/dev/null || true
+        zle zeno-auto-snippet-and-accept-line
+    }
+    zle -N _zeno-accept-line-clear-suggest
+    bindkey '^m' _zeno-accept-line-clear-suggest
 
-  bindkey '^i' zeno-completion
-  bindkey '^x^s' zeno-insert-snippet
-  bindkey '^x '  zeno-insert-space
-  bindkey '^x^m' accept-line
-  bindkey '^x^z' zeno-toggle-auto-snippet
-  bindkey '^r'   zeno-history-selection
+    bindkey '^i' zeno-completion
+    bindkey '^x^s' zeno-insert-snippet
+    bindkey '^x ' zeno-insert-space
+    bindkey '^x^m' accept-line
+    bindkey '^x^z' zeno-toggle-auto-snippet
+    bindkey '^r' zeno-history-selection
 fi
