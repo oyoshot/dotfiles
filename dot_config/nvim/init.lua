ee5963dba7caf4499d69d99bb31406a41b53f94b
oyoshot <oyoshot@gmail.com>
2026-08-29 07:41:01 +0900

    fix(nvim): disable unused legacy providers

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -4,6 +4,12 @@
 -- /_/_//_/_/\__(_)_/\_,_/\_,_/
 --
 
+-- Disable legacy remote-plugin providers that this config does not use.
+-- External commands such as `python3` remain available to plugins.
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 if vim.g.vscode then
 	require("core/vscode")
 else
