a76e548808e16d253efbc64edd9fb16e64e5ada9
oyoshot <oyoshot@gmail.com>
2026-08-30 01:06:46 +0900

    fix(nvim): initialize barbar watchers safely

--- dot_config/nvim/lua/plugins/barbar.lua
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -3,6 +3,12 @@ return {
 	event = { "BufReadPost", "BufAdd", "BufNewFile" },
 	init = function()
 		vim.g.barbar_auto_setup = false
+		-- barbar installs a watcher on this legacy option during setup.
+		-- Neovim 0.12 errors when its setup removes missing watchers, so create
+		-- the dictionary and the watchers that barbar replaces up front.
+		vim.g.bufferline = vim.empty_dict()
+		vim.cmd([[call dictwatcheradd(g:, 'bufferline', 'barbar#events#dict_changed')]])
+		vim.cmd([[call dictwatcheradd(g:bufferline, '*', 'barbar#events#on_option_changed')]])
 	end,
 	opts = {
 		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
